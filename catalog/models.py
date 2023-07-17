6967ed3a629abe0875d7db5e218e537e15561c8f
elecr0n4ik <andreyshka3@gmail.com>
2023-07-17 15:46:27 +0300

    Step 3

--- catalog/models.py
+++ catalog/models.py
@@ -1,15 +1,46 @@
 from django.db import models
 
-
-class Product(models.Model):
-    pass
-
-    def __str__(self):
-        return "self.name"
+NULLABLE = {
+    "null": True, "blank": True
+}
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100,
+                            **NULLABLE,
+                            verbose_name="Наименование")
+    description = models.CharField(max_length=200,
+                                   verbose_name="Описание")
 
     def __str__(self):
-        return "self.name"
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100,
+                            **NULLABLE,
+                            verbose_name="Наименование")
+    description = models.CharField(max_length=200,
+                                   verbose_name="Описание")
+    photo = models.ImageField(upload_to='catalog/',
+                              verbose_name="Фото")
+    category = models.ForeignKey(Category,
+                                 on_delete=models.CASCADE,
+                                 **NULLABLE,
+                                 verbose_name="Категория")
+    price = models.IntegerField(max_length=5,
+                                **NULLABLE,
+                                verbose_name="Цена")
+    data_factory = models.DateField()
+    data_update = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
