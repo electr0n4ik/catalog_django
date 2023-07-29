d0b03fb99c723fdd526b35134cf69c5d0714eb11
alectr0n4ik <andreyshka3@gmail.com>
2023-07-30 00:32:54 +0300

    fix

--- catalog/management/commands/put_data.py
+++ catalog/management/commands/put_data.py
@@ -10,19 +10,19 @@ class Command(BaseCommand):
         products = Product.objects.all()
         products.delete()
 
+        category_list = [
+            {'name': 'Фрукты', 'description': 'Обычно сладкие'},
+            {'name': 'Овощи', 'description': 'Растут на грядках'},
+        ]
+
         product_list = [
             {'name': 'Апельсин', 'description': 'Сладкий и оранжевый', "photo": "catalog/апельсин.png", "price": 100, "category_id": 1},
             {'name': 'Яблоко', 'description': 'Кислый и зеленый', "photo": "catalog/яблоко.jpeg", "price": 200, "category_id": 1},
             {'name': 'помидора', 'description': 'Красная', "photo": "catalog/помидора.png", "price": 300, "category_id": 2},
         ]
 
-        category_list = [
-            {'name': 'Фрукты', 'description': 'Обычно сладкие'},
-            {'name': 'Овощи', 'description': 'Растут на грядках'},
-        ]
+        for element in category_list:
+            Category.objects.create(**element)
 
         for element in product_list:
             Product.objects.create(**element)
-
-        for element in category_list:
-            Category.objects.create(**element)
