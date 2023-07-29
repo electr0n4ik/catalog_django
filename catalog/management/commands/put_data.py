f071030d46f28e8c0df5d82aaa634a8ea4add9a2
alectr0n4ik <andreyshka3@gmail.com>
2023-07-29 11:46:47 +0300

    fix

--- catalog/management/commands/put_data.py
+++ catalog/management/commands/put_data.py
@@ -11,9 +11,9 @@ class Command(BaseCommand):
         products.delete()
 
         product_list = [
-            {'name': 'Апельсин', 'description': 'Сладкий и оранжевый'},
-            {'name': 'Яблоко', 'description': 'Кислый и зеленый'},
-            {'name': 'Киви', 'description': 'Сладкий и зеленый'},
+            {'name': 'Апельсин', 'description': 'Сладкий и оранжевый', "photo": "catalog/апельсин.png", "price": 100, "category_id": 1},
+            {'name': 'Яблоко', 'description': 'Кислый и зеленый', "photo": "catalog/яблоко.jpeg", "price": 200, "category_id": 1},
+            {'name': 'помидора', 'description': 'Красная', "photo": "catalog/помидора.png", "price": 300, "category_id": 2},
         ]
 
         for element in product_list:
